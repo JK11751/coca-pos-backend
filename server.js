626df4809354a0ecc6bce9e680b32f91845f1a15
JK11751 <kgimase@gmail.com>
2025-02-02 14:29:07 +0300

    updated deps

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/api', productRoutes);
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
